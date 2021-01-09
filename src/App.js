1ff7739ee3a6364a35adcc08cc21544bda1c65a1
Thanh Cong Chien <thnhcngchin@gmail.com>
2021-01-09 22:35:00 +0700

    reupload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import AccountAction from "./containers/AccountAction/AccountAction";
+import {
+  BrowserRouter,
+  HashRouter,
+  Route,
+  Router,
+  Switch,
+} from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import history from "./history/history";
+import HashHistory from "./history/history";
+
+function App() {
+  // const history = createBrowserHistory({forceRefresh: true});
+  return (
+    <HashRouter basename={process.env.PUBLIC_URL} history={history}>
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/account"}
+            component={AccountAction}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/"}
+            component={HomePage}
+          />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
